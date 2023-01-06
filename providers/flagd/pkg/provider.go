69cb619b6cf0a3095ae0bb2f6544e22fb3d5786e
Skye Gill <gill.skye95@gmail.com>
2023-01-06 15:06:09 +0000

    feat: handle consolidated configuration change event (#66)
    Signed-off-by: Skye Gill <gill.skye95@gmail.com>

--- providers/flagd/pkg/provider.go
+++ providers/flagd/pkg/provider.go
@@ -482,17 +482,19 @@ func (p *Provider) handleConfigurationChangeEvent(ctx context.Context, event *sc
 		return errors.New("no data in event")
 	}
 
-	flagKeyVal, ok := event.Data.AsMap()["flagKey"]
+	flagsVal, ok := event.Data.AsMap()["flags"]
 	if !ok {
-		return errors.New("no flagKey field in event data")
+		return errors.New("no flags field in event data")
 	}
 
-	flagKey, ok := flagKeyVal.(string)
+	flags, ok := flagsVal.(map[string]interface{})
 	if !ok {
-		return errors.New("flagKey is not a string")
+		return errors.New("flags isn't a map")
 	}
 
-	p.cache.Remove(flagKey)
+	for flagKey := range flags {
+		p.cache.Remove(flagKey)
+	}
 
 	return nil
 }
