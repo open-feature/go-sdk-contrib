95729bd86f31d6adc617ba30e96de65a0c551b07
Kavindu Dodanduwa <kavindudodanduwa@gmail.com>
2024-03-15 09:33:59 -0700

    initial wiring of ofrep provider
    Signed-off-by: Kavindu Dodanduwa <kavindudodanduwa@gmail.com>

---
+++ providers/ofrep/go.mod
@@ -0,0 +1,10 @@
+module github.com/open-feature/go-sdk-contrib/providers/ofrep
+
+go 1.21.0
+
+require github.com/open-feature/go-sdk v1.10.0
+
+require (
+	github.com/go-logr/logr v1.4.1 // indirect
+	golang.org/x/exp v0.0.0-20240205201215-2c58cdc269a3 // indirect
+)
---
+++ providers/ofrep/internal/outbound/http.go
@@ -0,0 +1,57 @@
+package outbound
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"net/http"
+	"net/url"
+	"time"
+
+	of "github.com/open-feature/go-sdk/openfeature"
+)
+
+const ofrepV1 = "/ofrep/v1/evaluate/flags/"
+
+type AuthCallback func() (key string, value string)
+
+// Outbound client for http communication
+type Outbound struct {
+	auth    AuthCallback
+	baseURI string
+
+	client http.Client
+}
+
+func NewOutbound(baseUri string, callback AuthCallback) *Outbound {
+	client := http.Client{
+		Timeout: 10 * time.Second,
+	}
+
+	return &Outbound{
+		baseURI: baseUri,
+		client:  client,
+		auth:    callback,
+	}
+
+}
+
+func (h *Outbound) Post(ctx context.Context, key string, payload []byte) (*http.Response, error) {
+	path, err := url.JoinPath(h.baseURI, ofrepV1, key)
+	if err != nil {
+		return nil, fmt.Errorf("error building request path: %w", err)
+	}
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, path, bytes.NewReader(payload))
+	if err != nil {
+		resErr := of.NewGeneralResolutionError(fmt.Sprintf("request building error: %v", err))
+		return nil, &resErr
+	}
+
+	if h.auth != nil {
+		// set authentication headers
+		req.Header.Set(h.auth())
+	}
+
+	return h.client.Do(req)
+}
