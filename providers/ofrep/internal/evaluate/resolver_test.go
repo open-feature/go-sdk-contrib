a1cb699d4903502797a1184b79372b45ac1ef0b2
Kavindu Dodanduwa <Kavindu-Dodan@users.noreply.github.com>
2024-04-05 07:33:22 -0700

    feat: introduce OFREP provider   (#477)
    Signed-off-by: Kavindu Dodanduwa <kavindudodanduwa@gmail.com>

---
+++ providers/ofrep/internal/evaluate/resolver_test.go
@@ -0,0 +1,324 @@
+package evaluate
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"net/http"
+	"strings"
+	"testing"
+
+	"github.com/open-feature/go-sdk-contrib/providers/ofrep/internal/outbound"
+	of "github.com/open-feature/go-sdk/openfeature"
+)
+
+type resolverTest struct {
+	name   string
+	client mockOutbound
+}
+
+var success = evaluationSuccess{
+	Value:   true,
+	Key:     "flagA",
+	Reason:  string(of.StaticReason),
+	Variant: "true",
+	Metadata: map[string]interface{}{
+		"key": "value",
+	},
+}
+
+var successWithInvalidMetadata = evaluationSuccess{
+	Value:    true,
+	Key:      "flagA",
+	Reason:   string(of.StaticReason),
+	Variant:  "true",
+	Metadata: "metadata",
+}
+
+func TestSuccess200(t *testing.T) {
+	t.Run("success evaluation response", func(t *testing.T) {
+		successBytes, err := json.Marshal(success)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		resolver := OutboundResolver{client: mockOutbound{
+			rsp: outbound.Resolution{
+				Status: http.StatusOK,
+				Data:   successBytes,
+			},
+		}}
+
+		successDto, resolutionError := resolver.resolveSingle(context.Background(), "", make(map[string]interface{}))
+
+		if resolutionError != nil {
+			t.Errorf("expected no errors, but got error: %v", err)
+		}
+
+		if successDto == nil {
+			t.Fatal("expected non empty success response")
+		}
+
+		if successDto.Value != success.Value {
+			t.Errorf("expected value %v, but got %v", success.Value, successDto.Value)
+		}
+
+		if successDto.Variant != success.Variant {
+			t.Errorf("expected variant %v, but got %v", success.Variant, successDto.Variant)
+		}
+
+		if successDto.Reason != success.Reason {
+			t.Errorf("expected reason %s, but got %s", success.Reason, successDto.Reason)
+		}
+
+		if successDto.Metadata["key"] != "value" {
+			t.Errorf("expected key to contain value %s, but got %s", "value", successDto.Metadata["key"])
+		}
+	})
+
+	t.Run("invalid payload type results in general error", func(t *testing.T) {
+		resolver := OutboundResolver{client: mockOutbound{
+			rsp: outbound.Resolution{
+				Status: http.StatusOK,
+				Data:   []byte("some payload"),
+			},
+		}}
+		success, resolutionError := resolver.resolveSingle(context.Background(), "", make(map[string]interface{}))
+
+		validateErrorCode(success, resolutionError, of.GeneralCode, t)
+	})
+
+	t.Run("invalid metadata results in a parsing error", func(t *testing.T) {
+		b, err := json.Marshal(successWithInvalidMetadata)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		resolver := OutboundResolver{client: mockOutbound{
+			rsp: outbound.Resolution{
+				Status: http.StatusOK,
+				Data:   b,
+			},
+		}}
+		success, resolutionError := resolver.resolveSingle(context.Background(), "", make(map[string]interface{}))
+
+		validateErrorCode(success, resolutionError, of.ParseErrorCode, t)
+	})
+}
+
+func TestResolveGeneralErrors(t *testing.T) {
+	tests := []resolverTest{
+		{
+			name: "http error results in a general error",
+			client: mockOutbound{
+				err: errors.New("some http error"),
+				rsp: outbound.Resolution{},
+			},
+		},
+		{
+			name: "non ofrep http status codes results in general error",
+			client: mockOutbound{
+				rsp: outbound.Resolution{
+					Status: http.StatusServiceUnavailable,
+				},
+			},
+		},
+		{
+			name: "http 401",
+			client: mockOutbound{
+				rsp: outbound.Resolution{
+					Status: http.StatusUnauthorized,
+				},
+			},
+		},
+		{
+			name: "http 403",
+			client: mockOutbound{
+				rsp: outbound.Resolution{
+					Status: http.StatusForbidden,
+				},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			resolver := OutboundResolver{client: test.client}
+			success, resolutionError := resolver.resolveSingle(context.Background(), "key", map[string]interface{}{})
+
+			validateErrorCode(success, resolutionError, of.GeneralCode, t)
+		})
+	}
+}
+
+func TestEvaluationError4xx(t *testing.T) {
+	tests := []struct {
+		name       string
+		errorCode  of.ErrorCode
+		expectCode of.ErrorCode
+	}{
+		{
+			name:       "validate parse error",
+			errorCode:  of.ParseErrorCode,
+			expectCode: of.ParseErrorCode,
+		},
+		{
+			name:       "validate targeting key missing error",
+			errorCode:  of.TargetingKeyMissingCode,
+			expectCode: of.TargetingKeyMissingCode,
+		},
+		{
+			name:       "validate invalid context error",
+			errorCode:  of.InvalidContextCode,
+			expectCode: of.InvalidContextCode,
+		},
+		{
+			name:       "validate general error",
+			errorCode:  of.GeneralCode,
+			expectCode: of.GeneralCode,
+		},
+		{
+			name:       "validate ofrep unhandled code",
+			errorCode:  of.ProviderNotReadyCode,
+			expectCode: of.GeneralCode,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			// derive test specific error response
+			errBytes, err := json.Marshal(evaluationError{
+				ErrorCode: string(test.errorCode),
+			})
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			resolver := OutboundResolver{client: mockOutbound{
+				rsp: outbound.Resolution{
+					Status: http.StatusBadRequest,
+					Data:   errBytes,
+				},
+			}}
+			success, resolutionError := resolver.resolveSingle(context.Background(), "", make(map[string]interface{}))
+
+			validateErrorCode(success, resolutionError, test.expectCode, t)
+		})
+	}
+}
+
+func TestFlagNotFound404(t *testing.T) {
+	resolver := OutboundResolver{client: mockOutbound{
+		rsp: outbound.Resolution{
+			Status: http.StatusNotFound,
+		},
+	}}
+	success, resolutionError := resolver.resolveSingle(context.Background(), "", make(map[string]interface{}))
+
+	validateErrorCode(success, resolutionError, of.FlagNotFoundCode, t)
+}
+
+func Test429(t *testing.T) {
+	tests := []struct {
+		name       string
+		retryAfter string
+	}{
+		{
+			name:       "handle 429 with retry after header with seconds",
+			retryAfter: "10",
+		},
+		{
+			name:       "handle 429 with retry after header with date",
+			retryAfter: "Wed, 21 Oct 2015 07:28:00 GMT",
+		},
+		{
+			name: "handle 429 without retry header",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			// derive test specific handler
+			response := outbound.Resolution{
+				Status: http.StatusTooManyRequests,
+			}
+
+			if test.retryAfter != "" {
+				response.Headers = map[string][]string{
+					"Retry-After": {test.retryAfter},
+				}
+			}
+
+			resolver := OutboundResolver{client: mockOutbound{rsp: response}}
+			success, resolutionError := resolver.resolveSingle(context.Background(), "", make(map[string]interface{}))
+
+			validateErrorCode(success, resolutionError, of.GeneralCode, t)
+		})
+	}
+}
+
+func TestEvaluationError5xx(t *testing.T) {
+	t.Run("without body", func(t *testing.T) {
+		resolver := OutboundResolver{client: mockOutbound{
+			rsp: outbound.Resolution{
+				Status: http.StatusInternalServerError,
+				Data:   []byte{},
+			},
+		}}
+		success, resolutionError := resolver.resolveSingle(context.Background(), "", make(map[string]interface{}))
+
+		validateErrorCode(success, resolutionError, of.GeneralCode, t)
+	})
+
+	t.Run("with valid body", func(t *testing.T) {
+		errorBytes, err := json.Marshal(errorResponse{ErrorDetails: "some error detail"})
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		resolver := OutboundResolver{client: mockOutbound{
+			rsp: outbound.Resolution{
+				Status: http.StatusInternalServerError,
+				Data:   errorBytes,
+			},
+		}}
+		success, resolutionError := resolver.resolveSingle(context.Background(), "", make(map[string]interface{}))
+
+		validateErrorCode(success, resolutionError, of.GeneralCode, t)
+	})
+
+	t.Run("with invalid body", func(t *testing.T) {
+		resolver := OutboundResolver{client: mockOutbound{
+			rsp: outbound.Resolution{
+				Status: http.StatusInternalServerError,
+				Data:   []byte("some error"),
+			},
+		}}
+		success, resolutionError := resolver.resolveSingle(context.Background(), "", make(map[string]interface{}))
+
+		validateErrorCode(success, resolutionError, of.GeneralCode, t)
+	})
+}
+
+func validateErrorCode(success *successDto, resolutionError *of.ResolutionError, errorCode of.ErrorCode, t *testing.T) {
+	if success != nil {
+		t.Fatal("expected no success result, but got non nil value")
+	}
+
+	if resolutionError == nil {
+		t.Fatal("expected non nil error, but got empty")
+	}
+
+	if !strings.Contains(resolutionError.Error(), string(errorCode)) {
+		t.Errorf("expected error to contain error code %s", errorCode)
+	}
+}
+
+type mockOutbound struct {
+	err error
+	rsp outbound.Resolution
+}
+
+func (m mockOutbound) Single(_ context.Context, _ string, _ []byte) (*outbound.Resolution, error) {
+	return &m.rsp, m.err
+}
---
+++ providers/ofrep/internal/outbound/http_test.go
@@ -0,0 +1,89 @@
+package outbound
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+	"testing"
+	"time"
+)
+
+func TestHttpOutbound(t *testing.T) {
+	// given
+	host := "localhost:18181"
+	key := "flag"
+
+	server := http.Server{
+		Addr: host,
+		Handler: mockHandler{
+			t:   t,
+			key: key,
+		},
+	}
+
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil && !errors.Is(err, http.ErrServerClosed) {
+			t.Logf("error starting mock server: %v", err)
+			return
+		}
+	}()
+
+	<-time.After(3 * time.Second)
+
+	outbound := NewHttp(Configuration{
+		Callbacks: []HeaderCallback{
+			func() (string, string) {
+				return "Authorization", "Token"
+			},
+		},
+		BaseURI: fmt.Sprintf("http://%s", host),
+	})
+
+	// when
+	response, err := outbound.Single(context.Background(), key, []byte{})
+	if err != nil {
+		t.Fatalf("error from request: %v", err)
+		return
+	}
+
+	// then - expect an ok response
+	if response.Status != http.StatusOK {
+		t.Errorf("expected 200, but got %d", response.Status)
+	}
+
+	// cleanup
+	err = server.Shutdown(context.Background())
+	if err != nil {
+		t.Errorf("error shuttting down mock server: %v", err)
+	}
+}
+
+type mockHandler struct {
+	key string
+	t   *testing.T
+}
+
+func (r mockHandler) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodPost {
+		r.t.Logf("invalid request method, expected %s, got %s. test will fail", http.MethodPost, req.Method)
+		resp.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	path := fmt.Sprintf("%s%s", ofrepV1, r.key)
+	if req.RequestURI != fmt.Sprintf("%s%s", ofrepV1, r.key) {
+		r.t.Logf("invalid request path, expected %s, got %s. test will fail", path, req.RequestURI)
+		resp.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	if req.Header.Get("Authorization") == "" {
+		r.t.Log("expected non-empty Authorization header, but got empty. test will fail")
+		resp.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	resp.WriteHeader(http.StatusOK)
+}
