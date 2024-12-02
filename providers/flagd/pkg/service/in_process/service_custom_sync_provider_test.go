02e8bf49af8cf68b8d827db60fbfc3259ada4747
Maks Osowski <maks@google.com>
2024-12-02 11:10:43 +0000

    chore(flagd): add tests (incl. a fake sync.ISync implementation)
    Signed-off-by: Maks Osowski <maks@google.com>

---
+++ providers/flagd/pkg/service/in_process/service_custom_sync_provider_test.go
@@ -0,0 +1,15 @@
+package process
+
+import (
+	"testing"
+)
+
+func TestInProcessWithCustomSyncProvider(t *testing.T) {
+	customSyncProvider := NewDoNothingCustomSyncProvider()
+	service := NewInProcessService(Configuration{CustomSyncProvider: customSyncProvider, CustomSyncProviderUri: "not tested here"})
+
+	// If custom sync provider is supplied the in-process service should use it.
+	if service.sync != customSyncProvider {
+		t.Fatalf("Expected service.sync to be the mockCustomSyncProvider, but got %s", service.sync)
+	}
+}
