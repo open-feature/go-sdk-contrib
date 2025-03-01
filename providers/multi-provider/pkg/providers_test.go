70878e517ecd569a86f75d68c10decd9e9e5e2fe
bbland1 <104288486+bbland1@users.noreply.github.com>
2025-03-01 00:09:12 -0500

    added test checking if error thrown for unique name if not unique
    Signed-off-by: bbland1 <104288486+bbland1@users.noreply.github.com>

--- providers/multi-provider/pkg/providers_test.go
+++ providers/multi-provider/pkg/providers_test.go
@@ -31,7 +31,7 @@ func TestNewMultiProvider_ProviderMetadataUniqueNames(t *testing.T) {
 
 	multiProvider, err := NewMultiProvider([]UniqueNameProvider{
 		{
-			Provider:   testProvider1,
+			Provider: testProvider1,
 		}, {
 			Provider: testProvider2,
 		},
@@ -79,12 +79,12 @@ func TestNewMultiProvider_DuplicateProviderGenerateUniqueNames(t *testing.T) {
 
 	multiProvider, err := NewMultiProvider([]UniqueNameProvider{
 		{
-			Provider:   testProvider1,
+			Provider: testProvider1,
 		}, {
 			Provider: testProvider2,
 		}, {
 			Provider: testProvider3,
-		},{
+		}, {
 			Provider: testProvider4,
 		},
 	}, "test", defaultLogger)
@@ -112,22 +112,9 @@ func TestNewMultiProvider_DuplicateProviderGenerateUniqueNames(t *testing.T) {
 		t.Errorf("Expected there to be 4 provider entries, got: '%d'", len(providerEntries))
 	}
 }
-func TestNewMultiProvider_(t *testing.T) {
-	testProvider1 := memprovider.NewInMemoryProvider(map[string]memprovider.InMemoryFlag{
-		"boolFlag": {
-			Key:            "boolFlag",
-			State:          memprovider.Enabled,
-			DefaultVariant: "true",
-			Variants: map[string]interface{}{
-				"true":  true,
-				"false": false,
-			},
-			ContextEvaluator: nil,
-		},
-	})
+func TestNewMultiProvider_ProvidersUsePassedNames(t *testing.T) {
+	testProvider1 := oft.NewTestProvider()
 	testProvider2 := oft.NewTestProvider()
-	testProvider3 := oft.NewTestProvider()
-	testProvider4 := oft.NewTestProvider()
 
 	defaultLogger, err := hooks.NewLoggingHook(false)
 	if err != nil {
@@ -137,12 +124,10 @@ func TestNewMultiProvider_(t *testing.T) {
 	multiProvider, err := NewMultiProvider([]UniqueNameProvider{
 		{
 			Provider:   testProvider1,
+			UniqueName: "theFirst",
 		}, {
-			Provider: testProvider2,
-		}, {
-			Provider: testProvider3,
-		},{
-			Provider: testProvider4,
+			Provider:   testProvider2,
+			UniqueName: "theSecond",
 		},
 	}, "test", defaultLogger)
 
@@ -150,16 +135,44 @@ func TestNewMultiProvider_(t *testing.T) {
 		t.Errorf("Expected the multiprovider to successfully make an instance, '%s'", err)
 	}
 
-	if multiProvider.providersEntries[0].UniqueName != "InMemoryProvider" {
-		t.Errorf("Expected unique provider name to be: 'InMemoryProvider', got: '%s'", multiProvider.providersEntries[0].UniqueName)
+	providerEntries := multiProvider.Providers()
+
+	if providerEntries[0].UniqueName != "theFirst" {
+		t.Errorf("Expected unique provider name to be: 'theFirst', got: '%s'", providerEntries[0].UniqueName)
 	}
-	if multiProvider.providersEntries[1].UniqueName != "NoopProvider-1" {
-		t.Errorf("Expected unique provider name to be: 'NoopProvider', got: '%s'", multiProvider.providersEntries[1].UniqueName)
+	if providerEntries[1].UniqueName != "theSecond" {
+		t.Errorf("Expected unique provider name to be: 'theSecond', got: '%s'", providerEntries[1].UniqueName)
 	}
-	if multiProvider.providersEntries[2].UniqueName != "NoopProvider-2" {
-		t.Errorf("Expected unique provider name to be: 'NoopProvider', got: '%s'", multiProvider.providersEntries[2].UniqueName)
+
+	if len(providerEntries) != 2 {
+		t.Errorf("Expected there to be 2 provider entries, got: '%d'", len(providerEntries))
 	}
-	if multiProvider.providersEntries[3].UniqueName != "NoopProvider-3" {
-		t.Errorf("Expected unique provider name to be: 'NoopProvider', got: '%s'", multiProvider.providersEntries[3].UniqueName)
+}
+
+func TestNewMultiProvider_ProvidersErrorNameNotUnique(t *testing.T) {
+	testProvider1 := oft.NewTestProvider()
+	testProvider2 := oft.NewTestProvider()
+
+	defaultLogger, err := hooks.NewLoggingHook(false)
+	if err != nil {
+		t.Errorf("Issue setting up logger,'%s'", err)
+	}
+
+	_, err = NewMultiProvider([]UniqueNameProvider{
+		{
+			Provider:   testProvider1,
+			UniqueName: "provider",
+		}, {
+			Provider:   testProvider2,
+			UniqueName: "provider",
+		},
+	}, "test", defaultLogger)
+
+	if err == nil {
+		t.Errorf("Expected the multiprovider to have an error")
+	}
+
+	if err.Error() != "provider names must be unique" {
+		t.Errorf("Expected the multiprovider to have an error of: '%s', got: '%s'", errUniqueName, err.Error())
 	}
 }
