a3ba348c11f29b2e860e8baebc21d508f749dcd2
bbland1 <104288486+bbland1@users.noreply.github.com>
2025-03-05 14:25:17 -0500

    added test for getter methods of the struct provider entries
    Signed-off-by: bbland1 <104288486+bbland1@users.noreply.github.com>

--- providers/multi-provider/pkg/providers_test.go
+++ providers/multi-provider/pkg/providers_test.go
@@ -9,6 +9,200 @@ import (
 	oft "github.com/open-feature/go-sdk/openfeature/testing"
 )
 
+func TestMultiProvider_ProvidersMethod(t *testing.T) {
+	testProvider1 := oft.NewTestProvider()
+	testProvider2 := oft.NewTestProvider()
+
+	defaultLogger, err := hooks.NewLoggingHook(false)
+	if err != nil {
+		t.Errorf("Issue setting up logger,'%s'", err)
+	}
+
+	mp, err := NewMultiProvider([]UniqueNameProvider{
+		{
+			Provider:   testProvider1,
+			UniqueName: "provider1",
+		}, {
+			Provider:   testProvider2,
+			UniqueName: "provider2",
+		},
+	}, "test", defaultLogger)
+
+	if err != nil {
+		t.Errorf("Expected the multiprovider to successfully make an instance, '%s'", err)
+	}
+
+	providers := mp.Providers()
+
+	if len(providers) != 2 {
+		t.Errorf("Expected there to be '2' providers as passed but got: '%d'", len(providers))
+	}
+
+	if providers[0].UniqueName != "provider1" {
+		t.Errorf("Expected unique provider name to be: 'provider1', got: '%s'", providers[0].UniqueName)
+	}
+	if providers[1].UniqueName != "provider2" {
+		t.Errorf("Expected unique provider name to be: 'provider2', got: '%s'", providers[1].UniqueName)
+	}
+}
+
+func TestMultiProvider_ProvidersByNamesMethod(t *testing.T) {
+	testProvider1 := oft.NewTestProvider()
+	testProvider2 := oft.NewTestProvider()
+
+	defaultLogger, err := hooks.NewLoggingHook(false)
+	if err != nil {
+		t.Errorf("Issue setting up logger,'%s'", err)
+	}
+
+	mp, err := NewMultiProvider([]UniqueNameProvider{
+		{
+			Provider:   testProvider1,
+			UniqueName: "provider1",
+		}, {
+			Provider:   testProvider2,
+			UniqueName: "provider2",
+		},
+	}, "test", defaultLogger)
+
+	if err != nil {
+		t.Errorf("Expected the multiprovider to successfully make an instance, '%s'", err)
+	}
+
+	providers := mp.ProvidersByName()
+
+	if len(providers) != 2 {
+		t.Errorf("Expected there to be '2' providers as passed but got: '%d'", len(providers))
+	}
+
+	if provider, exists := providers["provider1"]; exists {
+		if provider.UniqueName != "provider1" {
+			t.Errorf("Expected unique provider name to be: 'provider1', got: '%s'", provider.UniqueName)
+		}
+		if provider.Provider != testProvider1 {
+			t.Errorf("Expected unique provider name to be: 'provider1', got: '%s'", provider.UniqueName)
+		}
+	} else {
+		t.Errorf("Expected there to be a provider with the key of '%s', but none was found.", "provider1")
+	}
+
+	if provider, exists := providers["provider2"]; exists {
+		if provider.UniqueName != "provider2" {
+			t.Errorf("Expected unique provider name to be: 'provider2', got: '%s'", provider.UniqueName)
+		}
+		if provider.Provider != testProvider2 {
+			t.Errorf("Expected unique provider name to be: 'provider2', got: '%s'", provider.UniqueName)
+		}
+	} else {
+		t.Errorf("Expected there to be a provider with the key of '%s', but none was found.", "provider2")
+	}
+
+}
+
+func TestMultiProvider_ProviderByNameMethod(t *testing.T) {
+	testProvider1 := oft.NewTestProvider()
+	testProvider2 := oft.NewTestProvider()
+
+	defaultLogger, err := hooks.NewLoggingHook(false)
+	if err != nil {
+		t.Errorf("Issue setting up logger,'%s'", err)
+	}
+
+	mp, err := NewMultiProvider([]UniqueNameProvider{
+		{
+			Provider:   testProvider1,
+			UniqueName: "provider1",
+		}, {
+			Provider:   testProvider2,
+			UniqueName: "provider2",
+		},
+	}, "test", defaultLogger)
+
+	if err != nil {
+		t.Errorf("Expected the multiprovider to successfully make an instance, '%s'", err)
+	}
+
+	providers := mp.ProvidersByName()
+	
+	if len(providers) != 2 {
+		t.Errorf("Expected there to be '2' providers as passed but got: '%d'", len(providers))
+	}
+	if provider, exists := mp.ProviderByName("provider2"); exists {
+		if provider.UniqueName != "provider2" {
+			t.Errorf("Expected unique provider name to be: 'provider2', got: '%s'", provider.UniqueName)
+		}
+		if provider.Provider != testProvider2 {
+			t.Errorf("Expected unique provider name to be: 'provider2', got: '%s'", provider.UniqueName)
+		}
+	} else {
+		t.Errorf("Expected there to be a provider with the key of '%s', but none was found.", "provider1")
+	}
+
+}
+
+// todo: currently the `multiProvider.Metadata()` just give the `Name` of the multi provider it doesn't aggregate the passed providers as stated in this specification https://openfeature.dev/specification/appendix-a/#metadata so this test fails
+func TestMultiProvider_MetaData(t *testing.T) {
+	testProvider1 := oft.NewTestProvider()
+	testProvider2 := oft.NewTestProvider()
+
+	defaultLogger, err := hooks.NewLoggingHook(false)
+	if err != nil {
+		t.Errorf("Issue setting up logger,'%s'", err)
+	}
+
+	mp, err := NewMultiProvider([]UniqueNameProvider{
+		{
+			Provider:   testProvider1,
+			UniqueName: "provider1",
+		}, {
+			Provider:   testProvider2,
+			UniqueName: "provider2",
+		},
+	}, "test", defaultLogger)
+
+	if err != nil {
+		t.Errorf("Expected the multiprovider to successfully make an instance, '%s'", err)
+	}
+
+	expectedMetadata := MultiMetadata{
+		Name: "multiprovider",
+		OriginalMetadata: map[string]openfeature.Metadata{
+			"provider1": openfeature.Metadata{Name: "NoopProvider"},
+			"provider2": openfeature.Metadata{Name: "NoopProvider"},
+		},
+	}
+
+	if mp.Metadata().Name != "hi" {
+		t.Errorf("Expected to see the aggregated metadata of all passed providers: '%s', got: '%s'", expectedMetadata, mp.Metadata().Name)
+	}
+}
+
+// func TestMultiProvider_Init(t *testing.T) {
+// 	testProvider1 := oft.NewTestProvider()
+// 	testProvider2 := oft.NewTestProvider()
+
+// 	defaultLogger, err := hooks.NewLoggingHook(false)
+// 	if err != nil {
+// 		t.Errorf("Issue setting up logger,'%s'", err)
+// 	}
+
+// 	mp, err := NewMultiProvider([]UniqueNameProvider{
+// 		{
+// 			Provider:   testProvider1,
+// 			UniqueName: "provider1",
+// 		}, {
+// 			Provider:   testProvider2,
+// 			UniqueName: "provider2",
+// 		},
+// 	}, "test", defaultLogger)
+
+// 	if err != nil {
+// 		t.Errorf("Expected the multiprovider to successfully make an instance, '%s'", err)
+// 	}
+
+// 	mp.Init()
+// }
+
 func TestNewMultiProvider_ProviderUniqueNames(t *testing.T) {
 	testProvider1 := memprovider.NewInMemoryProvider(map[string]memprovider.InMemoryFlag{
 		"boolFlag": {
@@ -29,7 +223,7 @@ func TestNewMultiProvider_ProviderUniqueNames(t *testing.T) {
 		t.Errorf("Issue setting up logger,'%s'", err)
 	}
 
-	multiProvider, err := NewMultiProvider([]UniqueNameProvider{
+	mp, err := NewMultiProvider([]UniqueNameProvider{
 		{
 			Provider: testProvider1,
 		}, {
@@ -41,7 +235,7 @@ func TestNewMultiProvider_ProviderUniqueNames(t *testing.T) {
 		t.Errorf("Expected the multiprovider to successfully make an instance, '%s'", err)
 	}
 
-	providerEntries := multiProvider.Providers()
+	providerEntries := mp.Providers()
 
 	if providerEntries[0].UniqueName != "InMemoryProvider" {
 		t.Errorf("Expected unique provider name to be: 'InMemoryProvider', got: '%s'", providerEntries[0].UniqueName)
@@ -77,7 +271,7 @@ func TestNewMultiProvider_DuplicateProviderGenerateUniqueNames(t *testing.T) {
 		t.Errorf("Issue setting up logger,'%s'", err)
 	}
 
-	multiProvider, err := NewMultiProvider([]UniqueNameProvider{
+	mp, err := NewMultiProvider([]UniqueNameProvider{
 		{
 			Provider: testProvider1,
 		}, {
@@ -93,7 +287,7 @@ func TestNewMultiProvider_DuplicateProviderGenerateUniqueNames(t *testing.T) {
 		t.Errorf("Expected the multiprovider to successfully make an instance, '%s'", err)
 	}
 
-	providerEntries := multiProvider.Providers()
+	providerEntries := mp.Providers()
 
 	if providerEntries[0].UniqueName != "InMemoryProvider" {
 		t.Errorf("Expected unique provider name to be: 'InMemoryProvider', got: '%s'", providerEntries[0].UniqueName)
@@ -121,7 +315,7 @@ func TestNewMultiProvider_ProvidersUsePassedNames(t *testing.T) {
 		t.Errorf("Issue setting up logger,'%s'", err)
 	}
 
-	multiProvider, err := NewMultiProvider([]UniqueNameProvider{
+	mp, err := NewMultiProvider([]UniqueNameProvider{
 		{
 			Provider:   testProvider1,
 			UniqueName: "theFirst",
@@ -135,7 +329,7 @@ func TestNewMultiProvider_ProvidersUsePassedNames(t *testing.T) {
 		t.Errorf("Expected the multiprovider to successfully make an instance, '%s'", err)
 	}
 
-	providerEntries := multiProvider.Providers()
+	providerEntries := mp.Providers()
 
 	if providerEntries[0].UniqueName != "theFirst" {
 		t.Errorf("Expected unique provider name to be: 'theFirst', got: '%s'", providerEntries[0].UniqueName)
@@ -174,43 +368,5 @@ func TestNewMultiProvider_ProvidersErrorNameNotUnique(t *testing.T) {
 
 	if err.Error() != "provider names must be unique" {
 		t.Errorf("Expected the multiprovider to have an error of: '%s', got: '%s'", errUniqueName, err.Error())
-	}
-}
-
-// todo: currently the `multiProvider.Metadata()` just give the `Name` of the multi provider it doesn't aggregate the passed providers as stated in this specification https://openfeature.dev/specification/appendix-a/#metadata so this test fails
-func TestAggregatedMetaData(t *testing.T){
-	testProvider1 := oft.NewTestProvider()
-	testProvider2 := oft.NewTestProvider()
-
-	defaultLogger, err := hooks.NewLoggingHook(false)
-	if err != nil {
-		t.Errorf("Issue setting up logger,'%s'", err)
-	}
-
-	multiProvider, err := NewMultiProvider([]UniqueNameProvider{
-		{
-			Provider:   testProvider1,
-			UniqueName: "provider1",
-		}, {
-			Provider:   testProvider2,
-			UniqueName: "provider2",
-		},
-	}, "test", defaultLogger)
-
-	if err != nil {
-		t.Errorf("Expected the multiprovider to successfully make an instance, '%s'", err)
-	}
-
-	expectedMetadata := MultiMetadata{
-		Name: "multiprovider",
-		OriginalMetadata: map[string]openfeature.Metadata{
-			"provider1": openfeature.Metadata{Name:"NoopProvider"},
-			"provider2": openfeature.Metadata{Name:"NoopProvider"},
-
-		},
-	}
-
-	if multiProvider.Metadata().Name != "hi" {
-		t.Errorf("Expected to see the aggregated metadata of all passed providers: '%s', got: '%s'", expectedMetadata, multiProvider.Metadata().Name)
 	}
 }
