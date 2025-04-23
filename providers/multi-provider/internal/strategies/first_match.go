99a2b1ddc258e3a71be022a5c85b40157c14d6d9
Jordan Blacker <jblacker@justworks.com>
2025-04-23 16:53:08 -0400

    refactor: Use evaluator for first_match
    Signed-off-by: Jordan Blacker <jblacker@justworks.com>

--- providers/multi-provider/internal/strategies/first_match.go
+++ providers/multi-provider/internal/strategies/first_match.go
@@ -21,81 +21,95 @@ func (f *FirstMatchStrategy) Name() EvaluationStrategy {
 }
 
 func (f *FirstMatchStrategy) BooleanEvaluation(ctx context.Context, flag string, defaultValue bool, evalCtx of.FlattenedContext) of.BoolResolutionDetail {
-	return *evaluateFirstMatch[of.BoolResolutionDetail](ctx, f.providers, flag, defaultValue, evalCtx).result
+	evalFunc := func(c context.Context, p *NamedProvider) resultWrapper[of.BoolResolutionDetail] {
+		r := p.Provider.BooleanEvaluation(c, flag, defaultValue, evalCtx)
+		return resultWrapper[of.BoolResolutionDetail]{
+			result: &r,
+			name:   p.Name,
+			value:  r.Value,
+			detail: r.ProviderResolutionDetail,
+		}
+	}
+	result := evaluateFirstMatch[of.BoolResolutionDetail](ctx, f.providers, evalFunc, defaultValue)
+	result.result.ProviderResolutionDetail = result.detail
+	return *result.result
 }
 
 func (f *FirstMatchStrategy) StringEvaluation(ctx context.Context, flag string, defaultValue string, evalCtx of.FlattenedContext) of.StringResolutionDetail {
-	return *evaluateFirstMatch[of.StringResolutionDetail](ctx, f.providers, flag, defaultValue, evalCtx).result
+	evalFunc := func(c context.Context, p *NamedProvider) resultWrapper[of.StringResolutionDetail] {
+		r := p.Provider.StringEvaluation(c, flag, defaultValue, evalCtx)
+		return resultWrapper[of.StringResolutionDetail]{
+			result: &r,
+			name:   p.Name,
+			value:  r.Value,
+			detail: r.ProviderResolutionDetail,
+		}
+	}
+	result := evaluateFirstMatch[of.StringResolutionDetail](ctx, f.providers, evalFunc, defaultValue)
+	result.result.ProviderResolutionDetail = result.detail
+	return *result.result
 }
 
 func (f *FirstMatchStrategy) FloatEvaluation(ctx context.Context, flag string, defaultValue float64, evalCtx of.FlattenedContext) of.FloatResolutionDetail {
-	return *evaluateFirstMatch[of.FloatResolutionDetail](ctx, f.providers, flag, defaultValue, evalCtx).result
+	evalFunc := func(c context.Context, p *NamedProvider) resultWrapper[of.FloatResolutionDetail] {
+		r := p.Provider.FloatEvaluation(c, flag, defaultValue, evalCtx)
+		return resultWrapper[of.FloatResolutionDetail]{
+			result: &r,
+			name:   p.Name,
+			value:  r.Value,
+			detail: r.ProviderResolutionDetail,
+		}
+	}
+	result := evaluateFirstMatch[of.FloatResolutionDetail](ctx, f.providers, evalFunc, defaultValue)
+	result.result.ProviderResolutionDetail = result.detail
+	return *result.result
 }
 
 func (f *FirstMatchStrategy) IntEvaluation(ctx context.Context, flag string, defaultValue int64, evalCtx of.FlattenedContext) of.IntResolutionDetail {
-	return *evaluateFirstMatch[of.IntResolutionDetail](ctx, f.providers, flag, defaultValue, evalCtx).result
+	evalFunc := func(c context.Context, p *NamedProvider) resultWrapper[of.IntResolutionDetail] {
+		r := p.Provider.IntEvaluation(c, flag, defaultValue, evalCtx)
+		return resultWrapper[of.IntResolutionDetail]{
+			result: &r,
+			name:   p.Name,
+			value:  r.Value,
+			detail: r.ProviderResolutionDetail,
+		}
+	}
+	result := evaluateFirstMatch[of.IntResolutionDetail](ctx, f.providers, evalFunc, defaultValue)
+	result.result.ProviderResolutionDetail = result.detail
+	return *result.result
 }
 
 func (f *FirstMatchStrategy) ObjectEvaluation(ctx context.Context, flag string, defaultValue interface{}, evalCtx of.FlattenedContext) of.InterfaceResolutionDetail {
-	return *evaluateFirstMatch[of.InterfaceResolutionDetail](ctx, f.providers, flag, defaultValue, evalCtx).result
+	evalFunc := func(c context.Context, p *NamedProvider) resultWrapper[of.InterfaceResolutionDetail] {
+		r := p.Provider.ObjectEvaluation(c, flag, defaultValue, evalCtx)
+		return resultWrapper[of.InterfaceResolutionDetail]{
+			result: &r,
+			name:   p.Name,
+			value:  r.Value,
+			detail: r.ProviderResolutionDetail,
+		}
+	}
+	result := evaluateFirstMatch[of.InterfaceResolutionDetail](ctx, f.providers, evalFunc, defaultValue)
+	result.result.ProviderResolutionDetail = result.detail
+	return *result.result
 }
 
-func evaluateFirstMatch[R resultConstraint, DV bool | string | int64 | float64 | interface{}](ctx context.Context, providers []*NamedProvider, flag string, defaultValue DV, evalCtx of.FlattenedContext) resultWrapper[R] {
+func evaluateFirstMatch[R resultConstraint, DV bool | string | int64 | float64 | interface{}](ctx context.Context, providers []*NamedProvider, e evaluator[R], defaultVal DV) resultWrapper[R] {
 	for _, provider := range providers {
-		switch any(defaultValue).(type) {
-		case bool:
-			r := provider.Provider.BooleanEvaluation(ctx, flag, any(defaultValue).(bool), evalCtx)
-			if r.Error() != nil && r.ResolutionDetail().ErrorCode == of.FlagNotFoundCode {
-				continue
-			} else if r.Error() != nil {
-				return buildDefaultResult[R](StrategyFirstMatch, defaultValue, r.Error())
-			}
-			rp := &r
-			rp.FlagMetadata = setFlagMetadata(StrategyFirstMatch, provider.Name, r.FlagMetadata)
-			return resultWrapper[R]{result: any(rp).(*R)}
-		case string:
-			r := provider.Provider.StringEvaluation(ctx, flag, any(defaultValue).(string), evalCtx)
-			if r.Error() != nil && r.ResolutionDetail().ErrorCode == of.FlagNotFoundCode {
-				continue
-			} else if r.Error() != nil {
-				return buildDefaultResult[R](StrategyFirstMatch, defaultValue, r.Error())
-			}
-			rp := &r
-			rp.FlagMetadata = setFlagMetadata(StrategyFirstMatch, provider.Name, r.FlagMetadata)
-			return resultWrapper[R]{result: any(rp).(*R)}
-		case int64:
-			r := provider.Provider.IntEvaluation(ctx, flag, any(defaultValue).(int64), evalCtx)
-			if r.Error() != nil && r.ResolutionDetail().ErrorCode == of.FlagNotFoundCode {
-				continue
-			} else if r.Error() != nil {
-				return buildDefaultResult[R](StrategyFirstMatch, defaultValue, r.Error())
-			}
-			rp := &r
-			rp.FlagMetadata = setFlagMetadata(StrategyFirstMatch, provider.Name, r.FlagMetadata)
-			return resultWrapper[R]{result: any(rp).(*R)}
-		case float64:
-			r := provider.Provider.FloatEvaluation(ctx, flag, any(defaultValue).(float64), evalCtx)
-			if r.Error() != nil && r.ResolutionDetail().ErrorCode == of.FlagNotFoundCode {
-				continue
-			} else if r.Error() != nil {
-				return buildDefaultResult[R](StrategyFirstMatch, defaultValue, r.Error())
-			}
-			rp := &r
-			rp.FlagMetadata = setFlagMetadata(StrategyFirstMatch, provider.Name, r.FlagMetadata)
-			return resultWrapper[R]{result: any(rp).(*R)}
-		default:
-			r := provider.Provider.ObjectEvaluation(ctx, flag, defaultValue, evalCtx)
-			if r.Error() != nil && r.ResolutionDetail().ErrorCode == of.FlagNotFoundCode {
-				continue
-			} else if r.Error() != nil {
-				return buildDefaultResult[R](StrategyFirstMatch, defaultValue, r.Error())
-			}
-			rp := &r
-			rp.FlagMetadata = setFlagMetadata(StrategyFirstMatch, provider.Name, r.FlagMetadata)
-			return resultWrapper[R]{result: any(rp).(*R)}
+		r := e(ctx, provider)
+		if r.detail.Error() != nil && r.detail.ResolutionDetail().ErrorCode == of.FlagNotFoundCode {
+			continue
 		}
+		if r.detail.Error() != nil {
+			return buildDefaultResult[R](StrategyFirstMatch, defaultVal, r.detail.Error())
+		}
+
+		// success!
+		r.detail.FlagMetadata = setFlagMetadata(StrategyFirstMatch, provider.Name, r.detail.FlagMetadata)
+		return r
 	}
 
 	// Build a default result if no matches are found
-	return buildDefaultResult[R](StrategyFirstMatch, defaultValue, nil)
+	return buildDefaultResult[R](StrategyFirstMatch, defaultVal, nil)
 }
