6c43c6bdf72d6dd59e1cce6ab46dc1a593b0e3ab
Jordan Blacker <jblacker@justworks.com>
2025-04-24 12:34:55 -0400

    doc: document options
    Signed-off-by: Jordan Blacker <jblacker@justworks.com>

--- providers/multi-provider/pkg/options.go
+++ providers/multi-provider/pkg/options.go
@@ -7,18 +7,21 @@ import (
 	"time"
 )
 
+// WithLogger Sets a logger to be used with slog for internal logging
 func WithLogger(l *slog.Logger) Option {
 	return func(conf *Configuration) {
 		conf.logger = l
 	}
 }
 
+// WithTimeout Set a timeout for the total runtime for evaluation of parallel strategies
 func WithTimeout(d time.Duration) Option {
 	return func(conf *Configuration) {
 		conf.timeout = d
 	}
 }
 
+// WithFallbackProvider Sets a fallback provider when using the StrategyComparison
 func WithFallbackProvider(p of.FeatureProvider) Option {
 	return func(conf *Configuration) {
 		conf.fallbackProvider = p
@@ -33,12 +36,14 @@ func WithCustomStrategy(s strategies.Strategy) Option {
 	}
 }
 
+// WithEventPublishing Enables event publishing (Not Yet Implemented)
 func WithEventPublishing() Option {
 	return func(conf *Configuration) {
 		conf.publishEvents = true
 	}
 }
 
+// WithoutEventPublishing Disables event publishing (this is the default, but included for explicit usage)
 func WithoutEventPublishing() Option {
 	return func(conf *Configuration) {
 		conf.publishEvents = false
