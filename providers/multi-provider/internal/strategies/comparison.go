19ccefe1198b9e38ebeacd01650d8bb3d032107a
Jordan Blacker <jblacker@justworks.com>
2025-04-23 20:59:50 -0400

    refactor: Modify comparison implementation with comp functions
    Signed-off-by: Jordan Blacker <jblacker@justworks.com>

--- providers/multi-provider/internal/strategies/comparison.go
+++ providers/multi-provider/internal/strategies/comparison.go
@@ -6,7 +6,14 @@ import (
 	"errors"
 	of "github.com/open-feature/go-sdk/openfeature"
 	"golang.org/x/sync/errgroup"
+	"slices"
 	"strings"
+)
+
+const (
+	MetadataIsDefault       = "multiprovider-is-default-result"
+	MetadataNoneFound       = "multiprovider-flag-not-found-all-providers"
+	MetadataEvaluationError = "multiprovider-comparison-first-error"
 )
 
 type (
@@ -15,7 +22,7 @@ type (
 		fallbackProvider of.FeatureProvider
 	}
 
-	evaluator[R resultConstraint] func(ctx context.Context, p *NamedProvider) resultWrapper[R]
+	comparator[R bool | string | int64 | float64] func(values []R) bool
 )
 
 var _ Strategy = (*ComparisonStrategy)(nil)
@@ -41,22 +48,28 @@ func (c ComparisonStrategy) BooleanEvaluation(ctx context.Context, flag string, 
 			detail: result.ProviderResolutionDetail,
 		}
 	}
-	results, metadata := evaluateComparison[of.BoolResolutionDetail, bool](ctx, c.providers, evalFunc, c.fallbackProvider, defaultValue)
-	if len(results) == 1 {
-		results[0].result.FlagMetadata[MetadataSuccessfulProviderName] = results[0].name
-		return *results[0].result
-	}
+	compFunc := func(values []bool) bool {
+		current := values[0]
+		match := true
+		for i, v := range values {
+			if i == 0 {
+				continue
+			}
+			if current != v {
+				match = false
+				break
+			}
+		}
 
-	reason := ReasonAggregated
-	if fallbackUsed, ok := metadata[MetadataFallbackUsed].(bool); fallbackUsed && ok {
-		reason = ReasonAggregatedFallback
+		return match
 	}
+	results, metadata := evaluateComparison[of.BoolResolutionDetail, bool](ctx, c.providers, evalFunc, compFunc, c.fallbackProvider, defaultValue)
 
 	return of.BoolResolutionDetail{
 		Value: results[0].result.Value,
 		ProviderResolutionDetail: of.ProviderResolutionDetail{
-			ResolutionError: of.ResolutionError{},
-			Reason:          reason,
+			ResolutionError: comparisonResolutionError(metadata),
+			Reason:          comparisonResolutionReason(metadata),
 			Variant:         results[0].detail.Variant,
 			FlagMetadata:    metadata,
 		},
@@ -73,22 +86,28 @@ func (c ComparisonStrategy) StringEvaluation(ctx context.Context, flag string, d
 			detail: result.ProviderResolutionDetail,
 		}
 	}
-	results, metadata := evaluateComparison[of.StringResolutionDetail, string](ctx, c.providers, evalFunc, c.fallbackProvider, defaultValue)
-	if len(results) == 1 {
-		results[0].result.FlagMetadata[MetadataSuccessfulProviderName] = results[0].name
-		return *results[0].result
+	compFunc := func(values []string) bool {
+		current := values[0]
+		match := true
+		for i, v := range values {
+			if i == 0 {
+				continue
+			}
+			if current != v {
+				match = false
+				break
+			}
+		}
+
+		return match
 	}
 
-	reason := ReasonAggregated
-	if fallbackUsed, ok := metadata[MetadataFallbackUsed].(bool); fallbackUsed && ok {
-		reason = ReasonAggregatedFallback
-	}
-
+	results, metadata := evaluateComparison[of.StringResolutionDetail, string](ctx, c.providers, evalFunc, compFunc, c.fallbackProvider, defaultValue)
 	return of.StringResolutionDetail{
 		Value: results[0].result.Value,
 		ProviderResolutionDetail: of.ProviderResolutionDetail{
-			ResolutionError: of.ResolutionError{},
-			Reason:          reason,
+			ResolutionError: comparisonResolutionError(metadata),
+			Reason:          comparisonResolutionReason(metadata),
 			Variant:         results[0].detail.Variant,
 			FlagMetadata:    metadata,
 		},
@@ -105,22 +124,28 @@ func (c ComparisonStrategy) FloatEvaluation(ctx context.Context, flag string, de
 			detail: result.ProviderResolutionDetail,
 		}
 	}
-	results, metadata := evaluateComparison[of.FloatResolutionDetail, float64](ctx, c.providers, evalFunc, c.fallbackProvider, defaultValue)
-	if len(results) == 1 {
-		results[0].result.FlagMetadata[MetadataSuccessfulProviderName] = results[0].name
-		return *results[0].result
+	compFunc := func(values []float64) bool {
+		current := values[0]
+		match := true
+		for i, v := range values {
+			if i == 0 {
+				continue
+			}
+			if current != v {
+				match = false
+				break
+			}
+		}
+
+		return match
 	}
 
-	reason := ReasonAggregated
-	if fallbackUsed, ok := metadata[MetadataFallbackUsed].(bool); fallbackUsed && ok {
-		reason = ReasonAggregatedFallback
-	}
-
+	results, metadata := evaluateComparison[of.FloatResolutionDetail, float64](ctx, c.providers, evalFunc, compFunc, c.fallbackProvider, defaultValue)
 	return of.FloatResolutionDetail{
 		Value: results[0].result.Value,
 		ProviderResolutionDetail: of.ProviderResolutionDetail{
-			ResolutionError: of.ResolutionError{},
-			Reason:          reason,
+			ResolutionError: comparisonResolutionError(metadata),
+			Reason:          comparisonResolutionReason(metadata),
 			Variant:         results[0].detail.Variant,
 			FlagMetadata:    metadata,
 		},
@@ -137,26 +162,57 @@ func (c ComparisonStrategy) IntEvaluation(ctx context.Context, flag string, defa
 			detail: result.ProviderResolutionDetail,
 		}
 	}
-	results, metadata := evaluateComparison[of.IntResolutionDetail, int64](ctx, c.providers, evalFunc, c.fallbackProvider, defaultValue)
-	if len(results) == 1 {
-		results[0].result.FlagMetadata[MetadataSuccessfulProviderName] = results[0].name
-		return *results[0].result
-	}
+	compFunc := func(values []int64) bool {
+		current := values[0]
+		match := true
+		for i, v := range values {
+			if i == 0 {
+				continue
+			}
+			if current != v {
+				match = false
+				break
+			}
+		}
 
-	reason := ReasonAggregated
-	if fallbackUsed, ok := metadata[MetadataFallbackUsed].(bool); fallbackUsed && ok {
-		reason = ReasonAggregatedFallback
+		return match
 	}
-
+	results, metadata := evaluateComparison[of.IntResolutionDetail, int64](ctx, c.providers, evalFunc, compFunc, c.fallbackProvider, defaultValue)
 	return of.IntResolutionDetail{
 		Value: results[0].result.Value,
 		ProviderResolutionDetail: of.ProviderResolutionDetail{
-			ResolutionError: of.ResolutionError{},
-			Reason:          reason,
+			ResolutionError: comparisonResolutionError(metadata),
+			Reason:          comparisonResolutionReason(metadata),
 			Variant:         results[0].detail.Variant,
 			FlagMetadata:    metadata,
 		},
 	}
+}
+
+func comparisonResolutionReason(metadata of.FlagMetadata) of.Reason {
+	reason := ReasonAggregated
+	if fallbackUsed, err := metadata.GetBool(MetadataFallbackUsed); fallbackUsed && err == nil {
+		reason = ReasonAggregatedFallback
+	} else if defaultUsed, err := metadata.GetBool(MetadataIsDefault); defaultUsed && err == nil {
+		reason = of.DefaultReason
+	}
+	return reason
+}
+
+func comparisonResolutionError(metadata of.FlagMetadata) of.ResolutionError {
+	if isDefault, err := metadata.GetBool(MetadataIsDefault); err != nil || !isDefault {
+		return of.ResolutionError{}
+	}
+
+	if notFound, err := metadata.GetBool(MetadataNoneFound); err == nil && notFound {
+		return of.NewFlagNotFoundResolutionError("not found in any providers")
+	}
+
+	if evalErr, err := metadata.GetString(MetadataEvaluationError); evalErr != "" && err != nil {
+		return of.NewGeneralResolutionError(evalErr)
+	}
+
+	return of.NewGeneralResolutionError("comparison failure")
 }
 
 func (c ComparisonStrategy) ObjectEvaluation(ctx context.Context, flag string, defaultValue interface{}, evalCtx of.FlattenedContext) of.InterfaceResolutionDetail {
@@ -176,74 +232,114 @@ func (c ComparisonStrategy) ObjectEvaluation(ctx context.Context, flag string, d
 	}
 }
 
-func evaluateComparison[R resultConstraint, DV bool | string | int64 | float64](ctx context.Context, providers []*NamedProvider, e evaluator[R], fallbackProvider of.FeatureProvider, defaultVal DV) ([]resultWrapper[R], of.FlagMetadata) {
-	resultChan := make(chan resultWrapper[R])
-	ctx, cancel := context.WithCancel(ctx)
-	defer cancel()
+func evaluateComparison[R resultConstraint, DV bool | string | int64 | float64](ctx context.Context, providers []*NamedProvider, e evaluator[R], comp comparator[DV], fallbackProvider of.FeatureProvider, defaultVal DV) ([]resultWrapper[R], of.FlagMetadata) {
+	if len(providers) == 1 {
+		result := e(ctx, providers[0])
+		metadata := setFlagMetadata(StrategyComparison, cmp.Or(result.name, providers[0].Name), make(of.FlagMetadata))
+		metadata[MetadataFallbackUsed] = false
+		return []resultWrapper[R]{result}, metadata
+	}
+
+	resultChan := make(chan *resultWrapper[R], len(providers))
+	notFoundChan := make(chan interface{})
 	errGrp, ctx := errgroup.WithContext(ctx)
 	for _, provider := range providers {
-		p := provider
 		errGrp.Go(func() error {
-			result := e(ctx, p)
-			notFound := result.detail.ResolutionDetail().ErrorCode == of.FlagNotFoundCode
-			if result.detail.Error() != nil {
-				return &providerError{
-					providerName: p.Name,
-					err:          result.detail.Error(),
+			localChan := make(chan *resultWrapper[R])
+
+			go func(c context.Context, p *NamedProvider) {
+				result := e(c, p)
+				localChan <- &result
+			}(ctx, provider)
+
+			select {
+			case r := <-localChan:
+				notFound := r.detail.ResolutionDetail().ErrorCode == of.FlagNotFoundCode
+				if !notFound && r.detail.Error() != nil {
+					return &providerError{
+						providerName: r.name,
+						err:          r.detail.Error(),
+					}
 				}
+				if !notFound {
+					resultChan <- r
+				} else {
+					notFoundChan <- struct{}{}
+				}
+				return nil
+			case <-ctx.Done():
+				return nil
 			}
-			result.name = p.Name
-			if !notFound {
-				resultChan <- result
-			}
-			return nil
 		})
 	}
 
-	if err := errGrp.Wait(); err != nil {
-		result := buildDefaultResult[R, DV](StrategyComparison, defaultVal, err)
-		return []resultWrapper[R]{result}, result.detail.FlagMetadata
-	}
-
-	// Evaluate Results Are Equal
-	agreement := true
-	var resultVal DV
 	results := make([]resultWrapper[R], 0, len(providers))
-	metadata := make(of.FlagMetadata)
-	metadata[MetadataFallbackUsed] = false
-	metadata[MetadataStrategyUsed] = StrategyComparison
-	success := make([]string, 0, len(providers))
-	for r := range resultChan {
-		results = append(results, r)
-		current := *r.value.(*DV)
-		resultVal = cmp.Or(resultVal, current)
-		agreement = agreement && (resultVal == current)
-		if !agreement {
-			break
+	resultValues := make([]DV, 0, len(providers))
+	notFoundCount := 0
+	for {
+		select {
+		case <-ctx.Done():
+			// Error occurred
+			result := buildDefaultResult[R, DV](StrategyComparison, defaultVal, ctx.Err())
+			metadata := result.detail.FlagMetadata
+			metadata[MetadataFallbackUsed] = false
+			metadata[MetadataIsDefault] = true
+			metadata[MetadataEvaluationError] = ctx.Err().Error()
+			return []resultWrapper[R]{result}, metadata
+		case r := <-resultChan:
+			results = append(results, *r)
+			resultValues = append(resultValues, r.value.(DV))
+			if (len(results) + notFoundCount) == len(providers) {
+				goto continueComparison
+			}
+		case <-notFoundChan:
+			notFoundCount += 1
+			if notFoundCount == len(providers) {
+				result := buildDefaultResult[R, DV](StrategyComparison, defaultVal, ctx.Err())
+				metadata := result.detail.FlagMetadata
+				metadata[MetadataFallbackUsed] = false
+				metadata[MetadataIsDefault] = true
+				return []resultWrapper[R]{result}, metadata
+			}
+			if (len(results) + notFoundCount) == len(providers) {
+				goto continueComparison
+			}
 		}
-		metadata[r.name] = r.detail.FlagMetadata
-		success = append(success, r.name)
 	}
-	metadata[MetadataSuccessfulProviderName+"s"] = strings.Join(success, ", ")
-
+continueComparison:
+	// Evaluate Results Are Equal
+	metadata := make(of.FlagMetadata)
+	metadata[MetadataStrategyUsed] = StrategyComparison
+	agreement := comp(resultValues)
 	if agreement {
+		metadata[MetadataFallbackUsed] = false
+		metadata[MetadataIsDefault] = false
+		success := make([]string, 0, len(providers))
+		for _, r := range results {
+			metadata[r.name] = r.detail.FlagMetadata
+			success = append(success, r.name)
+		}
+		// maintain stable order of metadata results
+		slices.Sort(success)
+		metadata[MetadataSuccessfulProviderName+"s"] = strings.Join(success, ", ")
 		return results, metadata
 	}
 
 	if fallbackProvider != nil {
 		fallbackResult := e(ctx, &NamedProvider{Name: "fallback", Provider: fallbackProvider})
 		metadata = fallbackResult.detail.FlagMetadata
-		metadata[MetadataStrategyUsed] = StrategyComparison
 		metadata[MetadataFallbackUsed] = true
+		metadata[MetadataIsDefault] = false
+		metadata[MetadataSuccessfulProviderName] = "fallback"
+		metadata[MetadataStrategyUsed] = StrategyComparison
 
 		return []resultWrapper[R]{fallbackResult}, metadata
 	}
 
 	defaultResult := buildDefaultResult[R, DV](StrategyComparison, defaultVal, errors.New("no fallback provider configured"))
 	metadata = defaultResult.detail.FlagMetadata
-	metadata[MetadataSuccessfulProviderName] = "none"
 	metadata[MetadataFallbackUsed] = false
-	metadata[MetadataStrategyUsed] = StrategyComparison
-	return []resultWrapper[R]{defaultResult}, metadata
+	metadata[MetadataIsDefault] = true
 
+	return []resultWrapper[R]{defaultResult}, metadata
 }
