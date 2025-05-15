dd50b782eb8d8e7c1b4fab9bce918a75708d0fe0
Jordan Blacker <jblacker@justworks.com>
2025-05-14 21:41:43 -0400

    chore: regenerate mocks
    Signed-off-by: Jordan Blacker <jblacker@justworks.com>

--- providers/multi-provider/internal/mocks/mocks.go
+++ providers/multi-provider/internal/mocks/mocks.go
@@ -1,2 +1,2 @@
-//go:generate go run go.uber.org/mock/mockgen -destination=../../internal/mocks/openfeature_mocks.go -package=mocks  "github.com/open-feature/go-sdk/openfeature" FeatureProvider,Hook
+//go:generate go run go.uber.org/mock/mockgen -destination=../../internal/mocks/openfeature_mocks.go -package=mocks  "github.com/open-feature/go-sdk/openfeature" FeatureProvider,Hook,StateHandler,EventHandler
 package mocks
