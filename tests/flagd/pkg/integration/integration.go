ce871021d0c45d3c992bb00b33c8b7a8e337e9a3
Roman Dmytrenko <rdmytrenko@gmail.com>
2025-06-12 15:55:30 +0000

    fix(deps): bump open-feature/go-sdk from v1.11 to v1.15 (#686)
    Signed-off-by: Roman Dmytrenko <rdmytrenko@gmail.com>
    Co-authored-by: Sahid Velji <sahidvelji@gmail.com>

--- tests/flagd/pkg/integration/integration.go
+++ tests/flagd/pkg/integration/integration.go
@@ -15,12 +15,13 @@ type ctxStorageKey struct{}
 
 // ctxClientKey is the key used to pass the openfeature client across context.Context
 type ctxClientKey struct{}
+
 var domain = "flagd-e2e-tests"
 
 func aFlagdProviderIsSet(ctx context.Context) (context.Context, error) {
 	readyChan := make(chan struct{})
 
-	err := openfeature.SetNamedProvider(domain, test_provider_supplier())
+	err := openfeature.SetNamedProviderAndWait(domain, test_provider_supplier())
 	if err != nil {
 		return nil, err
 	}
