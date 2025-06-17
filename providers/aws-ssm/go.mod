be49da48d84359dc1857a0a08b4f016641e081d9
Giovanni De Giorgio <giovannidegiorgio1999@gmail.com>
2025-06-17 21:37:09 +0200

    feat: implement aws service
    Signed-off-by: Giovanni De Giorgio <giovannidegiorgio1999@gmail.com>

--- providers/aws-ssm/go.mod
+++ providers/aws-ssm/go.mod
@@ -5,6 +5,20 @@ go 1.24.3
 require github.com/open-feature/go-sdk v1.15.1
 
 require (
+	github.com/aws/aws-sdk-go-v2 v1.36.5 // indirect
+	github.com/aws/aws-sdk-go-v2/config v1.29.17 // indirect
+	github.com/aws/aws-sdk-go-v2/credentials v1.17.70 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.16.32 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.3.36 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.6.36 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.8.3 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.12.4 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.12.17 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssm v1.59.3 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.25.5 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.30.3 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.34.0 // indirect
+	github.com/aws/smithy-go v1.22.4 // indirect
 	github.com/go-logr/logr v1.4.3 // indirect
 	go.uber.org/mock v0.5.2 // indirect
 )
--- providers/aws-ssm/pkg/service/aws.go
+++ providers/aws-ssm/pkg/service/aws.go
@@ -1,13 +1,84 @@
 package service
 
+import (
+	"context"
+	"fmt"
 
-type AWSServiceOptions struct{
-	Region	string
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/ssm"
+	"github.com/open-feature/go-sdk/openfeature"
+)
+
+
+type AWS struct{
+	client	*ssm.Client
+	decryption bool
 }
 
-type AWS struct{}
+
+func NewAWSService() (*AWS, error){
+
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+
+    if err != nil {
+    	return nil, fmt.Errorf("could not initialize aws config : %v+", err)
+    }
+
+    client := ssm.NewFromConfig(cfg)
 
 
-func New(opts ...AWSServiceOptions) *AWS{
-	return &AWS{}
+    return &AWS{
+   		client : client,
+    }, nil
+}
+
+func (svc *AWS) ResolveBoolean(ctx context.Context, flag string, defaultValue bool, evalCtx openfeature.FlattenedContext) openfeature.BoolResolutionDetail {
+
+}
+
+
+func (svc *AWS) ResolveString(ctx context.Context, flag string, defaultValue string, evalCtx openfeature.FlattenedContext) openfeature.StringResolutionDetail {
+
+	param := &ssm.GetParameterInput{
+		Name: &flag,
+		WithDecryption: &svc.decryption,
+	}
+
+	res, err := svc.client.GetParameter( ctx, param)
+
+	if err != nil {
+		return openfeature.StringResolutionDetail{
+			Value: defaultValue,
+			ProviderResolutionDetail: openfeature.ProviderResolutionDetail{
+				Reason: openfeature.ErrorReason,
+				ResolutionError: openfeature.NewGeneralResolutionError(err.Error()),
+			},
+		}
+	}
+
+	return nil
+
+
+}
+
+
+func (svc *AWS) ResolveInt(ctx context.Context, flag string, defaultValue int64, evalCtx openfeature.FlattenedContext) openfeature.IntResolutionDetail {
+
+}
+
+
+func (svc *AWS) ResolveFloat(ctx context.Context, flag string, defaultValue float64, evalCtx openfeature.FlattenedContext) openfeature.FloatResolutionDetail {
+
+}
+
+
+
+func (svc *AWS) ResolveObject(ctx context.Context, flag string, defaultValue interface{}, evalCtx openfeature.FlattenedContext) openfeature.InterfaceResolutionDetail {
+
+}
+
+
+func (svc *AWS) WithDecryption(decryption bool) *AWS {
+	svc.decryption = decryption
+	return svc
 }
