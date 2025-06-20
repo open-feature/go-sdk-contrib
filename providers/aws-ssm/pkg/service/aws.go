9d1c61360dc64c1d7d303a7110538dd82c987488
Giovanni De Giorgio <giovannidegiorgio1999@gmail.com>
2025-06-20 12:30:58 +0200

    feat: implement aws service
    Signed-off-by: Giovanni De Giorgio <giovannidegiorgio1999@gmail.com>

--- providers/aws-ssm/pkg/service/aws.go
+++ providers/aws-ssm/pkg/service/aws.go
@@ -3,82 +3,210 @@ package service
 import (
 	"context"
 	"fmt"
+	"strconv"
 
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/ssm"
 	"github.com/open-feature/go-sdk/openfeature"
 )
 
-
-type AWS struct{
-	client	*ssm.Client
+type AWS struct {
+	client     *ssm.Client
 	decryption bool
 }
 
-
-func NewAWSService() (*AWS, error){
+func NewAWSService() (*AWS, error) {
 
 	cfg, err := config.LoadDefaultConfig(context.TODO())
 
-    if err != nil {
-    	return nil, fmt.Errorf("could not initialize aws config : %v+", err)
-    }
+	if err != nil {
+		return nil, fmt.Errorf("could not initialize aws config : %v+", err)
+	}
 
-    client := ssm.NewFromConfig(cfg)
+	client := ssm.NewFromConfig(cfg)
 
-
-    return &AWS{
-   		client : client,
-    }, nil
+	return &AWS{
+		client: client,
+	}, nil
 }
 
 func (svc *AWS) ResolveBoolean(ctx context.Context, flag string, defaultValue bool, evalCtx openfeature.FlattenedContext) openfeature.BoolResolutionDetail {
 
-}
-
-
-func (svc *AWS) ResolveString(ctx context.Context, flag string, defaultValue string, evalCtx openfeature.FlattenedContext) openfeature.StringResolutionDetail {
-
-	param := &ssm.GetParameterInput{
-		Name: &flag,
-		WithDecryption: &svc.decryption,
-	}
-
-	res, err := svc.client.GetParameter( ctx, param)
+	res, err := svc.getValueFromSSM(ctx, flag)
 
 	if err != nil {
-		return openfeature.StringResolutionDetail{
+		return openfeature.BoolResolutionDetail{
 			Value: defaultValue,
 			ProviderResolutionDetail: openfeature.ProviderResolutionDetail{
-				Reason: openfeature.ErrorReason,
+				Reason:          openfeature.ErrorReason,
 				ResolutionError: openfeature.NewGeneralResolutionError(err.Error()),
 			},
 		}
 	}
 
-	return nil
+	b, err := strconv.ParseBool(*res.Parameter.Value)
 
+	if err != nil {
+		return openfeature.BoolResolutionDetail{
+			Value: defaultValue,
+			ProviderResolutionDetail: openfeature.ProviderResolutionDetail{
+				Reason:          openfeature.ErrorReason,
+				ResolutionError: openfeature.NewGeneralResolutionError(err.Error()),
+			},
+		}
+	}
+
+	return openfeature.BoolResolutionDetail{
+		Value: b,
+		ProviderResolutionDetail: openfeature.ProviderResolutionDetail{
+			Reason: openfeature.StaticReason,
+			FlagMetadata: openfeature.FlagMetadata{
+				"SSMMetadata": res.ResultMetadata,
+			},
+		},
+	}
 
 }
 
+func (svc *AWS) ResolveString(ctx context.Context, flag string, defaultValue string, evalCtx openfeature.FlattenedContext) openfeature.StringResolutionDetail {
+
+	res, err := svc.getValueFromSSM(ctx, flag)
+
+	if err != nil {
+		return openfeature.StringResolutionDetail{
+			Value: defaultValue,
+			ProviderResolutionDetail: openfeature.ProviderResolutionDetail{
+				Reason:          openfeature.ErrorReason,
+				ResolutionError: openfeature.NewGeneralResolutionError(err.Error()),
+			},
+		}
+	}
+
+	return openfeature.StringResolutionDetail{
+		Value: *res.Parameter.Value,
+		ProviderResolutionDetail: openfeature.ProviderResolutionDetail{
+			Reason: openfeature.StaticReason,
+			FlagMetadata: openfeature.FlagMetadata{
+				"SSMMetadata": res.ResultMetadata,
+			},
+		},
+	}
+
+}
 
 func (svc *AWS) ResolveInt(ctx context.Context, flag string, defaultValue int64, evalCtx openfeature.FlattenedContext) openfeature.IntResolutionDetail {
+	res, err := svc.getValueFromSSM(ctx, flag)
 
+	if err != nil {
+		return openfeature.IntResolutionDetail{
+			Value: defaultValue,
+			ProviderResolutionDetail: openfeature.ProviderResolutionDetail{
+				Reason:          openfeature.ErrorReason,
+				ResolutionError: openfeature.NewGeneralResolutionError(err.Error()),
+			},
+		}
+	}
+
+	i, err := strconv.ParseInt(*res.Parameter.Value, 10, 64)
+
+	if err != nil {
+		return openfeature.IntResolutionDetail{
+			Value: defaultValue,
+			ProviderResolutionDetail: openfeature.ProviderResolutionDetail{
+				Reason:          openfeature.ErrorReason,
+				ResolutionError: openfeature.NewGeneralResolutionError(err.Error()),
+			},
+		}
+	}
+
+	return openfeature.IntResolutionDetail{
+		Value: i,
+		ProviderResolutionDetail: openfeature.ProviderResolutionDetail{
+			Reason: openfeature.StaticReason,
+			FlagMetadata: openfeature.FlagMetadata{
+				"SSMMetadata": res.ResultMetadata,
+			},
+		},
+	}
 }
-
 
 func (svc *AWS) ResolveFloat(ctx context.Context, flag string, defaultValue float64, evalCtx openfeature.FlattenedContext) openfeature.FloatResolutionDetail {
+	res, err := svc.getValueFromSSM(ctx, flag)
 
+	if err != nil {
+		return openfeature.FloatResolutionDetail{
+			Value: defaultValue,
+			ProviderResolutionDetail: openfeature.ProviderResolutionDetail{
+				Reason:          openfeature.ErrorReason,
+				ResolutionError: openfeature.NewGeneralResolutionError(err.Error()),
+			},
+		}
+	}
+
+	f, err := strconv.ParseFloat(*res.Parameter.Value, 64)
+
+	if err != nil {
+		return openfeature.FloatResolutionDetail{
+			Value: defaultValue,
+			ProviderResolutionDetail: openfeature.ProviderResolutionDetail{
+				Reason:          openfeature.ErrorReason,
+				ResolutionError: openfeature.NewGeneralResolutionError(err.Error()),
+			},
+		}
+	}
+
+	return openfeature.FloatResolutionDetail{
+		Value: f,
+		ProviderResolutionDetail: openfeature.ProviderResolutionDetail{
+			Reason: openfeature.StaticReason,
+			FlagMetadata: openfeature.FlagMetadata{
+				"SSMMetadata": res.ResultMetadata,
+			},
+		},
+	}
 }
-
-
 
 func (svc *AWS) ResolveObject(ctx context.Context, flag string, defaultValue interface{}, evalCtx openfeature.FlattenedContext) openfeature.InterfaceResolutionDetail {
+	res, err := svc.getValueFromSSM(ctx, flag)
 
+	if err != nil {
+		return openfeature.InterfaceResolutionDetail{
+			Value: defaultValue,
+			ProviderResolutionDetail: openfeature.ProviderResolutionDetail{
+				Reason:          openfeature.ErrorReason,
+				ResolutionError: openfeature.NewGeneralResolutionError(err.Error()),
+			},
+		}
+	}
+
+	return openfeature.InterfaceResolutionDetail{
+		Value: defaultValue,
+		ProviderResolutionDetail: openfeature.ProviderResolutionDetail{
+			Reason: openfeature.StaticReason,
+			FlagMetadata: openfeature.FlagMetadata{
+				"SSMMetadata": res.ResultMetadata,
+			},
+		},
+	}
 }
-
 
 func (svc *AWS) WithDecryption(decryption bool) *AWS {
 	svc.decryption = decryption
 	return svc
+}
+
+func (svc *AWS) getValueFromSSM(ctx context.Context, flag string) (*ssm.GetParameterOutput, error) {
+
+	param := &ssm.GetParameterInput{
+		Name:           &flag,
+		WithDecryption: &svc.decryption,
+	}
+
+	res, err := svc.client.GetParameter(ctx, param)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
 }
