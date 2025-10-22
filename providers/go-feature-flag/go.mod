b2f2413ea23bf0827a30a50fa56acdd4eeea7ed1
Roman Dmytrenko <rdmytrenko@gmail.com>
2025-10-22 12:40:22 +0100

    chore(go-feature-flag): bump minimum Go requirement to 1.24 and modernize
    Signed-off-by: Roman Dmytrenko <rdmytrenko@gmail.com>

--- providers/go-feature-flag/go.mod
+++ providers/go-feature-flag/go.mod
@@ -1,8 +1,6 @@
 module github.com/open-feature/go-sdk-contrib/providers/go-feature-flag
 
-go 1.23.0
-
-toolchain go1.24.4
+go 1.24.0
 
 require (
 	github.com/bluele/gcache v0.0.2
@@ -17,7 +15,6 @@ require (
 	github.com/kr/pretty v0.3.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	go.uber.org/mock v0.5.2 // indirect
-	golang.org/x/exp v0.0.0-20240506185415-9bf2ced13842 // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- providers/go-feature-flag/pkg/hook/evaluation_enrichment_hook.go
+++ providers/go-feature-flag/pkg/hook/evaluation_enrichment_hook.go
@@ -5,26 +5,26 @@ import (
 	"github.com/open-feature/go-sdk/openfeature"
 )
 
-func NewEvaluationEnrichmentHook(exporterMetadata map[string]interface{}) openfeature.Hook {
+func NewEvaluationEnrichmentHook(exporterMetadata map[string]any) openfeature.Hook {
 	return &evaluationEnrichmentHook{exporterMetadata: exporterMetadata}
 }
 
 type evaluationEnrichmentHook struct {
 	openfeature.UnimplementedHook
-	exporterMetadata map[string]interface{}
+	exporterMetadata map[string]any
 }
 
 func (d *evaluationEnrichmentHook) Before(_ context.Context, hookCtx openfeature.HookContext, _ openfeature.HookHints) (*openfeature.EvaluationContext, error) {
 	attributes := hookCtx.EvaluationContext().Attributes()
 	if goffSpecific, ok := attributes["gofeatureflag"]; ok {
 		switch typed := goffSpecific.(type) {
-		case map[string]interface{}:
+		case map[string]any:
 			typed["exporterMetadata"] = d.exporterMetadata
 		default:
-			attributes["gofeatureflag"] = map[string]interface{}{"exporterMetadata": d.exporterMetadata}
+			attributes["gofeatureflag"] = map[string]any{"exporterMetadata": d.exporterMetadata}
 		}
 	} else {
-		attributes["gofeatureflag"] = map[string]interface{}{"exporterMetadata": d.exporterMetadata}
+		attributes["gofeatureflag"] = map[string]any{"exporterMetadata": d.exporterMetadata}
 	}
 	newCtx := openfeature.NewEvaluationContext(hookCtx.EvaluationContext().TargetingKey(), attributes)
 	return &newCtx, nil
